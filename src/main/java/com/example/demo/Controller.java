c3561abf1d4c3830fc1ed46228e2e7fef8792175
alina-yur <alina.yurenko@oracle.com>
2024-06-06 17:25:22 +0200

    upd

--- src/main/java/com/example/demo/Controller.java
+++ src/main/java/com/example/demo/Controller.java
@@ -2,27 +2,23 @@ package com.example.demo;
 
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.stereotype.Controller;
-import org.springframework.ai.chat.ChatClient;
-import org.springframework.ai.chat.prompt.Prompt;
-import org.springframework.ai.chat.prompt.PromptTemplate;
 
 public class Controller {
     
     private final ChatClient chatClient;
 
-    public MyController(ChatClient.Builder chatClientBuilder) {
+    public Controller(ChatClient.Builder chatClientBuilder) {
         this.chatClient = chatClientBuilder
                             .defaultSystem("provide sentiment analysis for user input")
                             .build();
     }
 
-    @GetMapping("/ai")
+/*     @GetMapping("/ai")
     String generation(String userInput) {
         return this.chatClient.prompt()
             .user(userInput)
             .call()
             .content();
-    }
+    } */
 
 }
